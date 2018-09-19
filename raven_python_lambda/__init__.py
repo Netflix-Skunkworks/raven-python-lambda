57d793db57a2a5b793a15540c33e5bbfacb070bb
Daniel Ancuta <whisller@gmail.com>
2018-09-19 19:12:09 +0100

    Fix of the fix ;)

--- raven_python_lambda/__init__.py
+++ raven_python_lambda/__init__.py
@@ -192,7 +192,7 @@ class RavenLambdaWrapper(object):
                     if event.get('requestContext'):
                         breadcrumb['data'] = {
                             'http_method': event['requestContext']['httpMethod'],
-                            'host': event.get('headers', {}).get('Host'),
+                            'host': (event['headers'] or {}).get('Host'),
                             'path': event['path']
                         }
 
